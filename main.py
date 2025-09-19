fabaef11d4a325c0b2fd9fc6c00b9b70c11de8ef
Michal0002 <michal.kasperek2000@wp.pl>
2025-09-19 22:40:10 +0200

    Integrate HuggingFace summarizer model

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from transformers import pipeline
+
 def main():
     print("=== AI Chat Summarizer ===")
+
     text = input("👉 Paste text into summary: ")
-    print("\n📄 Received text:\n")
-    print(text)
+
+    print("\n Generating summary...\n")
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+    summary = summarizer(text, max_length=120, min_length=30, do_sample=False)
+
+    print("📌 Result:\n")
+    print(summary[0]['summary_text'])
+    
 if __name__ == "__main__":
     main()
